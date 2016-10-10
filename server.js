dd969b885c6eb6e28fa6878d8a5f6b4c045a77c7
cloud.admin <mika.rinne@oracle.com>
2016-10-10 14:04:32 +0000

    Merge branch '22-debug.fix' into 'master'. Resolved issues: 1) Task 22 - add debug.

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ function handleRequest(request, response, requestBody) {
 }
 
 server.listen(PORT, function () {
-  console.log('Server running on port ...');
+  console.log('Server running on port ...' + PORT);
 });
 
 function doRelease(connection)
